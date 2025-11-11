b659f93016de22f5654993a507fd1dd88434332e
alsa <als.salsa27@gmail.com>
2025-11-11 11:35:45 +0700

    upd alsa be done

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -5,8 +5,9 @@ import { Outlet } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadTheme } from '../features/themeSlice'
 import { Loader2Icon } from 'lucide-react'
-import { useUser, SignIn, useAuth} from '@clerk/clerk-react'
+import { useUser, SignIn, useAuth, CreateOrganization} from '@clerk/clerk-react'
 import { fetchWorkspaces } from '../features/workspaceSlice'
+import Dashboard from './Dashboard'
 
 
 const Layout = () => {
@@ -41,6 +42,14 @@ const Layout = () => {
             <Loader2Icon className="size-7 text-blue-500 animate-spin" />
         </div>
     )
+
+    if (user && workspaces.length === 0){
+        return (
+            <div className='min-h-screen flex justify-center items-center'>
+                <CreateOrganization />
+            </div>
+        )
+    }
 
     return (
         <div className="flex bg-white dark:bg-zinc-950 text-gray-900 dark:text-slate-100">
